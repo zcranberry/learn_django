690144a48e3734729c322448509ba9454a1928a9
zcranberry <zxdpku@126.com>
2015-09-19 15:02:18 +0800

    writing view that actually do something

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -7,6 +7,7 @@ import datetime
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
     def __unicode__(self):
         return self.question_text
 
@@ -18,6 +19,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __unicode__(self):
         return self.choice_text
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse(" Hello World, You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question_text for p in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at the question of question %s" % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
 
 # Create your views here.
