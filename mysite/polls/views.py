94c2ae521a3b411a021dda1b6a4168a18d822f4d
zcranberry <zxdpku@126.com>
2015-09-19 15:52:54 +0800

    can visit the proper detail page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.http import Http404,HttpResponse
+#from django.template import RequestContext, loader
+from django.shortcuts import render
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {'latest_question_list': latest_question_list})
-    return HttpResponse(template.render(context))
+#    template = loader.get_template('polls/index.html')
+#    context = RequestContext(request, {'latest_question_list': latest_question_list})
+    context = {'latest_question_list':latest_question_list}
+    #return HttpResponse(template.render(context))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at the question of question %s" % question_id)
+    try:
+        question = Question.objects.get(pk = question_id)
+    except Question.DoesNotExist:
+        raise Http404("Queestion does not exist")
+
+    return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
